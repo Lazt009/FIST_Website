652e619a71a623ed213f54eeba4e2200184dd640
Lazt009 <shinganems@rknec.edu>
2021-11-06 23:45:13 +0530

    profile form working and data is getting stored in database

--- js/profile.js
+++ js/profile.js
@@ -1,4 +1,8 @@
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.3.0/firebase-auth.js";
+import { getFirestore, collection, doc, setDoc, getDocs  } from "https://www.gstatic.com/firebasejs/9.3.0/firebase-firestore.js";
+
+
+const db = getFirestore();
 
 //get user info 
 const auth = getAuth();
@@ -10,7 +14,56 @@ onAuthStateChanged(auth, (user) => {
         document.querySelector("#inputEmail").value = user.email;
         document.querySelector("#inputName").value = user.displayName;
         document.querySelector("#inputUID").value = user.uid;
-        // Code to get things speak channel id
         
+        // code for configuring the form action
+        const formSubmit = document.querySelector("#form-submit");
+        formSubmit.addEventListener("click", (e) => {
+            e.preventDefault();
+            console.log("form submitted");
+            const cId = document.querySelector("#inputChannelID");
+
+            if( cId.value == "") {
+                alert("Please Input Channel ID");
+            }
+            else{
+                // console.log( cId.value );
+                // add channel id to the firestore database
+                const path = "channel-ids/" + user.uid;
+                // doc(db, path);
+                setDoc(doc(db, path), {
+                    channelId: cId.value
+                })
+                .then( () => {
+                    console.log("Channel Id Added");
+                    alert("Channel id Added");
+                })
+            }
+        });
+
+
+        // Code to get things speak channel id
+
+        var exists = false;
+        var data = null;
+        getDocs(collection(db, "channel-ids"))
+        .then( (response) => {
+            //check all responses 
+            response.forEach( (d) => {
+                console.log(d.id, user.uid);
+                if( d.id == user.uid ) {
+                    data = d.data()['channelId'];
+                    exists = true;
+                }
+            });
+
+            //if the channel id exists 
+            if( exists == true ) {
+                // console.log(data);
+                document.querySelector("#inputChannelID").value = data;
+            }
+            else{
+                console.log("User has not yet added the channel id data");
+            }
+        });
     }
 });
